d5f70e36c23528ac61ea0b1f4e1a1c9e23ca6f71
Simon Rogers <s.rogers@waxdigital.com>
2019-05-07 15:51:24 +0100

    change player attributes

---
+++ FMS3/Models/PlayerAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using FMS3.Enums;
+
+namespace FMS3.Models
+{
+    public class PlayerAttribute
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public int AttributeId { get; set; }
+        [Display(Name = "Rating")]
+        public int AttributeValue { get; set; }
+        [Display(Name = "Attribute")]
+        public string AttributeName => Enum.GetName(typeof(PlayerAttributeEnum), AttributeId);
+    }
+}
